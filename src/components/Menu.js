7e7574e14aa00672355c6d9c0c30e3a4c64bde15
Veli Can Aydın <37342494+velicanaydin0@users.noreply.github.com>
2023-02-15 14:20:51 +0300

    Update Menu.js
    ₺ eklendi

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,7 +10,7 @@ function Menu({ items }) {
               <div className="item-info">
                 <header>
                   <h4>{title} </h4>
-                  <h4 className="price">${price}</h4>
+                  <h4 className="price">{price}₺</h4>
                 </header>
                 <p className="item-text">{desc}</p>
               </div>
