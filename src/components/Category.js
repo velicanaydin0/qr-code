02cee65b5d48eb0507ace7552871b83e1b296154
Onur Varsak <onurvarsak@Onur-MacBook-Pro.local>
2023-02-18 22:32:49 +0300

    changed category design

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,33 +1,22 @@
-import { useEffect } from "react";
-
 function Category({ categories, filterItems, items }) {
   return (
-    <div className="btn-container">
-
-
+    <div className='btn-container'>
       {categories.map((category, i) => {
-        const firstItem = items.find(function (item){
+        const firstItem = items.find(function (item) {
           return item.category === category;
         });
 
-        if(firstItem)
-        return (
-        <div>
-          <button
-            type="button"
-            key={i}
-            onClick={() => filterItems(category)}
-            className="filter-btn"
-          >
-
-            <img width="100%" height="100%" src={firstItem.img} />
-            <br/>
-            {category}
-            <br/>
-          </button>
-          <br/>
-          </div>
-        );
+        if (firstItem)
+          return (
+            <div
+              onClick={() => filterItems(category)}
+              style={{ backgroundImage: firstItem.img }}
+              className='category-container'
+            >
+              <img src={firstItem.img} />
+              <span>{category}</span>
+            </div>
+          );
       })}
     </div>
   );
